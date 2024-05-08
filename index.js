4a2e547aafce7d662b7af81b55d230f4e1248ff5
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2024-05-08 14:23:19 -0300

    changing the port to the default https port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { createServer } from 'node:http'
 import { request } from 'node:https'
 import { parse } from 'node:url'
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
 const server = createServer(async (req, res) => {
   const requestedUrl = parse(req.url, true).query.url
